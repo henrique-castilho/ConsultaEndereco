685663bc5565303fa3faaaf01b4c881d0ee5682f
henrique-castilho <henrique.castilho.pires@gmail.com>
2025-04-24 15:50:59 -0300

    fix(p1): refatora integracao com viacep

--- ConsultaEndereco/src/components/App.jsx
+++ ConsultaEndereco/src/components/App.jsx
@@ -1,45 +1,16 @@
 import React from 'react'
 import Busca from './Busca'
 import LocalidadeLista from './LocalidadeLista'
-import viaCep from '../util/viaCep'
 
 
 class App extends React.Component {
-
-  onBuscaRealizada = (cep) => {
-  if(cep.trim()===""){
-    alert("O campo deve ser preenchido")
-    return
-  }
-  else{
-  viaCep.get(`${cep}/json`)
-  .then((response) => {
-      const dados = response.data; 
-      if(dados.erro){
-          alert("O cep não foi encontrado")
-          return
-      }
-      else{
-          console.log(dados);
-          alert("Funcionou")
-      }
-  })
-  .catch((erro) => {
-      alert("Insira somente os 8 numeros do cep")
-      console.error('Erro ao buscar CEP:', erro); 
-  }); 
-}
-};
-
   render() {
     return (
       <div className='container mt-4'>
         <div className="mb-4">
-          <Busca
-            onBuscaRealizada={this.onBuscaRealizada} 
-          />
-          <br/>
-          <LocalidadeLista/>
+          <Busca />
+          <br />
+          <LocalidadeLista />
         </div>
       </div>
     )
--- ConsultaEndereco/src/components/Busca.jsx
+++ ConsultaEndereco/src/components/Busca.jsx
@@ -2,6 +2,8 @@
 import React, { Component } from 'react'
 import { InputText } from 'primereact/inputtext'
 import { Button } from "primereact/button"
+import viaCep from '../util/viaCep'
+
 
 export class Busca extends Component {
     state = {
@@ -16,7 +18,39 @@ export class Busca extends Component {
 
     onFormSubmit = (event) => {
         event.preventDefault()
-        this.props.onBuscaRealizada(this.state.termoDeBusca)
+
+        if (this.state.termoDeBusca === "") {
+            alert("O campo deve ser preenchido")
+            return
+        }
+
+        if (this.state.termoDeBusca.length !== 8) {
+            alert('Por favor, digite um CEP válido com 8 dígitos.')
+            return
+        }
+
+        viaCep.get(`${this.state.termoDeBusca}/json`)
+            .then((response) => {
+                const dados = response.data
+
+                if (dados.erro) {
+                    alert("O cep não foi encontrado")
+                    return
+                }
+                console.log(dados)
+                alert("Funcionou")
+            })
+            .catch((erro) => {
+                alert("Erro ao buscar o CEP. Verifique sua conexão ou tente novamente mais tarde.")
+                console.error('Erro ao buscar CEP:', erro)
+            })
+    }
+
+    somenteNumeros = (event) => {
+        const asciiNumeros = event.charCode;
+        if (asciiNumeros < 48 || asciiNumeros > 57) {
+            event.preventDefault()
+        }
     }
 
     render() {
@@ -24,9 +58,10 @@ export class Busca extends Component {
             <form onSubmit={this.onFormSubmit}>
                 <div className='flex flex-column w-20rem'>
                     <InputText
-                        className='mb-2 w-full text-center' 
+                        className='mb-2 w-full text-center'
                         placeholder={this.props.dica}
                         maxLength={8}
+                        onKeyPress={this.somenteNumeros}
                         onChange={this.onTermoAlterado}
                         value={this.state.termoDeBusca}
                     />
