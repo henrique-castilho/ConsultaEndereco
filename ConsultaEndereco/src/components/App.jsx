7eecc5f8ef1711e8c28c6dc155eb1908917f0312
thediaax <gabrieldias23.dev@gmail.com>
2025-04-23 21:38:37 -0300

    feat(p1): integra com a api da viacep

--- ConsultaEndereco/src/components/App.jsx
+++ ConsultaEndereco/src/components/App.jsx
@@ -1,12 +1,35 @@
 import React from 'react'
 import Busca from './Busca'
 import LocalidadeLista from './LocalidadeLista'
+import viaCep from '../util/viaCep'
+
 
 class App extends React.Component {
 
-  onBuscaRealizada = (termoDigitado) => {
-    alert(termoDigitado)
+  onBuscaRealizada = (cep) => {
+  if(cep.trim()===""){
+    alert("O campo deve ser preenchido")
+    return
   }
+  else{
+  viaCep.get(`${cep}/json`)
+  .then((response) => {
+      const dados = response.data; 
+      if(dados.erro){
+          alert("O cep não foi encontrado")
+          return
+      }
+      else{
+          console.log(dados);
+          alert("Funcionou")
+      }
+  })
+  .catch((erro) => {
+      alert("Insira somente os 8 numeros do cep")
+      console.error('Erro ao buscar CEP:', erro); 
+  }); 
+}
+};
 
   render() {
     return (
